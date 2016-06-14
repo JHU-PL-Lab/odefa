e90f49b05fade8af7ee453c710deeb314b1eade9
Zachary Palmer <zep_git@bahj.com>
2016-06-14 15:53:56 -0400

    Added quasiquotation to dummy example for simplicity.

--- src/pds-programming/pds_programming.ml
+++ src/pds-programming/pds_programming.ml
@@ -1,31 +1,11 @@
 open Ast_mapper;;
-open Asttypes;;
-open Parsetree;;
-(*
-open Ast_helper;;
-open Location;;
-open Longident;;
-*)
 
 let dummy_mapper _ =
   { default_mapper with
     expr = fun mapper expr ->
       match expr with
-      | { pexp_desc = Pexp_extension( { txt = "dummy"; _ }, payload ); _ } ->
-        (match payload with
-         | PStr [
-             { pstr_desc = Pstr_eval({ pexp_desc = expr_desc'; _ }, attrs); _ }
-           ] ->
-           let expr' = { pexp_desc = expr_desc'
-                       ; pexp_loc = !Ast_helper.default_loc
-                       ; pexp_attributes = attrs
-                       }
-          in
-           default_mapper.expr mapper expr'
-         | PStr _ -> failwith "TODO"
-         | PTyp _ -> failwith "TODO"
-         | PPat (_,_) -> failwith "TODO")
-
+      | [%expr [%dummy [%e? expr']]] ->
+        mapper.expr mapper expr'
       | _ ->
         default_mapper.expr mapper expr
   }
