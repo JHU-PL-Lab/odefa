e7ec8d26ecb7a207d1cd3e8f62f1b95108b6c2c7
Achintya Gopal <achintyagopal@hotmail.com>
2017-10-25 15:30:14 -0400

    Remove forward creation of graph

--- src/core-interpreter/core_interpreter.ml
+++ src/core-interpreter/core_interpreter.ml
@@ -61,21 +61,21 @@ let initialize_graph cls =
       |> Utils.pairwise_enum_fold
         (fun acl1 acl2 -> Wddpac_edge(acl1,acl2))
     in 
-  (add_edges edges empty_graph, (Start_clause(None)))
+  (add_edges edges empty_graph, (End_clause rx))
 ;;
 
 let wire site_cl func x1 x2 graph =
   let site_acl = Unannotated_clause(site_cl) in
   let Function_value(x0, Expr(body)) = func in
   let wire_in_acl = Enter_clause(x0,x1,site_cl) in
-  let start_acl = Start_clause(Some(x0)) in
+  let wire_out_acl = Exit_clause(x2,rv body,site_cl) in
   if Wddpac_graph.has_succ wire_in_acl graph then (
       (* print_endline "Wire in acl already exists"; *)
-      (List.enum [], start_acl)
+      (List.enum [], wire_out_acl)
     )
   else 
+    let start_acl = Start_clause(Some(x0)) in
     let end_acl = End_clause (rv body) in
-    let wire_out_acl = Exit_clause(x2,rv body,site_cl) in
     let pred_acl = Wddpac_graph.direct_pred site_acl graph
     in
     let succ_acl = Wddpac_graph.direct_succ site_acl graph
@@ -92,13 +92,13 @@ let wire site_cl func x1 x2 graph =
       |> Utils.pairwise_enum_fold
         (fun acl1 acl2 -> Wddpac_edge(acl1,acl2))
     in
-    (edges, start_acl)
+    (edges, wire_out_acl)
 ;;
 
 let rec lookup graph var node lookup_stack context_stack = 
 
   let pred_option = Wddpac_graph.direct_pred_option node graph in
-  (* print_endline ("Var " ^ (show_var var)); *)
+  print_endline ("Var " ^ (show_var var));
   match node, pred_option with
   
   | Unannotated_clause(Clause(x, cl) as c), Some(pred) -> 
@@ -124,15 +124,16 @@ let rec lookup graph var node lookup_stack context_stack =
               (* print_endline "Value Discard"; *)
               lookup graph x1 pred (Lookup_Stack.pop lookup_stack) context_stack
           end
-        | Appl_body(xf, _) -> 
+        | Appl_body(xf, xv) -> 
           (* print_endline "Lookup function"; *)
           let fn = lookup graph xf pred Lookup_Stack.empty context_stack in
+          
           begin
             match fn with
-            | Value_function(Function_value(_, Expr(e))) ->
-                let x' = rv e in 
+            | Value_function(Function_value(_, Expr(_)) as fn) ->
+                let edges, exit_clause = wire c fn xv x graph in
                 (* print_endline "Lookup given function and return value"; *)
-                lookup graph var (Exit_clause(x, x', c)) lookup_stack context_stack
+                lookup (add_edges edges graph) var exit_clause lookup_stack context_stack
             | _ -> raise @@ Utils.Invariant_failure "Found incorrect definitions for function"
           end
         | _ -> raise @@ Utils.Invariant_failure "Usage of not implemented clause"
@@ -174,68 +175,11 @@ let rec lookup graph var node lookup_stack context_stack =
     (* print_endline "End_clause";   *)
     lookup graph var pred lookup_stack context_stack 
   | _, _ -> raise @@ Utils.Invariant_failure "Could not find valid predecessor node"
-
-;;
-
-let rec create_graph graph node context_stack = 
-  let succ_option = Wddpac_graph.direct_succ_option node graph in
-  match node, succ_option with
-  | Unannotated_clause(Clause(x1, cl) as sitecl), Some(succ) -> 
-    begin
-      match cl with
-      | Var_body(_) -> 
-        (* print_endline "Var body"; *)
-        create_graph graph succ context_stack
-      | Value_body(Value_function(_)) -> 
-        (* print_endline "Value body"; *)
-        create_graph graph succ context_stack
-      | Appl_body(x2, x3) -> 
-        (* print_endline "Appl body"; *)
-        (* print_endline "Lookup fn"; *)
-        (* print_endline (show_clause sitecl); *)
-        let fn = lookup graph x2 node Lookup_Stack.empty context_stack in 
-        (* print_endline "Lookup v"; *)
-        let _ = lookup graph x3 node Lookup_Stack.empty context_stack in 
-        (* print_endline "wire in clause"; *)
-        begin
-          match fn with
-          | Value_function(Function_value(_) as fn) ->  
-            let edges, start_clause = wire sitecl fn x3 x1 graph in
-            (* print_endline "Continue after adding edges and updating stack"; *)
-            create_graph ( (add_edges edges graph)) start_clause (Unbounded_Stack.push sitecl context_stack)
-          | _ -> raise @@ Utils.Invariant_failure "First parameter is application is not a function"
-        end
-      | _ -> raise @@ Utils.Invariant_failure "Usage of not implemented clause"
-    end
-
-  | Enter_clause(_, _, _), _ -> 
-    raise @@ Utils.Invariant_failure "Enter clause reached while creating graph"
-  | Exit_clause(_, _, _), Some(succ) ->
-    (* print_endline "Exit_clause"; *)
-    create_graph graph succ (Unbounded_Stack.pop context_stack)
-  | Start_clause(_), Some(succ) -> 
-    (* print_endline "Start_clause"; *)
-    create_graph graph succ context_stack
-  | End_clause(x'), None -> 
-    (* print_endline "End_clause"; *)
-    begin
-      match Unbounded_Stack.top context_stack with
-      | None -> (
-        (* print_endline "No successors and stack empty"; *)
-        graph
-      ) 
-      | Some(Clause(x, _) as c) -> (
-        (* print_endline "Continue through successors"; *)
-        create_graph graph (Exit_clause(x, x', c)) context_stack
-      )
-    end
-  | _, _ -> raise @@ Utils.Invariant_failure "Could not find valid successor node"
 ;;
 
 let eval (Expr(cls)) =
   let context_stack = Unbounded_Stack.empty in
   let lookup_stack = Lookup_Stack.empty in
-  let initial_graph, initial_node = initialize_graph cls in 
-  let complete_graph = create_graph initial_graph initial_node context_stack in
-  lookup complete_graph (rv cls) (End_clause(rv cls)) lookup_stack context_stack;
+  let initial_graph, final_node = initialize_graph cls in 
+  lookup initial_graph (rv cls) final_node lookup_stack context_stack;
 ;;
