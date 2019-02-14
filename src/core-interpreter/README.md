b1e35a7a8118f07778fac05c60dec9884f4f5ba4
Theodore Park <tedpark7@gmail.com>
2019-02-14 18:12:32 -0500

    functions done, conditionals started

--- src/core-interpreter/README.md
+++ src/core-interpreter/README.md
@@ -9,16 +9,11 @@ cp ddpa_graphs.log.json ddpa_graphs.json
 
 Symbolic_generator.ml will assume the ddpa_graphs.json file is the file is wants to use.
 
-
+Restrictions:
+a,b,c,d,e vars are assumed to be booleans
 
 Notes  
 
-lookup_stack is a (var * string) stack. What does the string represent? The context? if so why is it not just
-(var * annotated_clause) stack (or whatever type context is)?
+Just get conditionals working first, then go back and add in PI checks
 
-Didn't test negated - ddpa didn't like true
-
-Not sure what the purpose of context stack is now if we just annotated every var
-
-Just finished exit clause - not completely sure it's right
-working on parsing enter and exit clauses from ddpa cfg next
+Context annotated on variables is not really implemented at all
--- src/core-interpreter/symbolic_generator.ml
+++ src/core-interpreter/symbolic_generator.ml
@@ -4,7 +4,6 @@ open Jhupllib;;
 open Core_ast;;
 open Core_interpreter_utils;;
 
-(* open Sys;; *)
 open Unix;;
 open Yojson.Basic.Util;;
 
@@ -27,12 +26,23 @@ let script formulas_list =
   execv "/usr/bin/python" [| "python";"/home/theodore/research/odefa/src/core-interpreter/test.py";formulas_list|]
 ;;
 
+(* note that when we recurse into a function, we add the outermost level declarations first - so when we traverse the graph we go into the function first *)
 let rec make_map clauses graph : (var,clause_body) Hashtbl.t =
   match clauses with
   | [] -> graph
   | Clause(var, body) :: tail ->
-    Hashtbl.add graph var body;
-    make_map tail graph
+    match body with
+    | Value_body(Value_function(Function_value(_, Expr(fcn_body)))) ->
+      Hashtbl.add graph var body;
+      (* recurse into function body *)
+      make_map tail (make_map fcn_body graph)
+    | Conditional_body(_,_,Function_value(_, Expr(f1)),Function_value(_, Expr(f2))) ->
+      Hashtbl.add graph var body;
+      (* recurse into function bodies *)
+      make_map tail (make_map f2 (make_map f1 graph))
+    | _ ->
+      Hashtbl.add graph var body;
+      make_map tail graph
 ;;
 
 let get_annotated_clause_from_json (edge:Yojson.Basic.json list) map : annotated_clause =
@@ -57,15 +67,43 @@ let get_annotated_clause_from_json (edge:Yojson.Basic.json list) map : annotated
     let ident = to_string (List.nth ident_list 1) in
     let var = Var(Ident(ident), None) in
     (* get the node clause body from the map *)
-    let node_to_add = Hashtbl.find map var in
+    let node_to_add =
+      try
+        Hashtbl.find map var
+      with
+      | Not_found -> print_endline ("ident: " ^ ident); failwith "Var not found"
+    in
     Unannotated_clause(Clause(var, node_to_add))
   else if node_type = "Start_clause" then
-    let a = List.nth edge 1 |> to_list in
-    let b = List.nth a 1 |> to_list in
-    let var_ident = List.nth b 1 |> to_string in
+    let var_ident = List.nth (List.nth (List.nth edge 1 |> to_list) 1 |> to_list) 1 |> to_string in
     Start_clause(Core_ast.Var(Ident(var_ident),None))
   else if node_type = "End_clause" then
-    End_clause(Core_ast.Var(Ident("d"),None))
+    let var_ident = List.nth (List.nth (List.nth edge 1 |> to_list) 1 |> to_list) 1 |> to_string in
+    End_clause(Core_ast.Var(Ident(var_ident),None))
+  else if node_type = "Enter_clause" then
+    let param = List.nth (List.nth (List.nth edge 1 |> to_list) 1 |> to_list) 1 |> to_string in
+    let arg = List.nth (List.nth (List.nth edge 2 |> to_list) 1 |> to_list) 1 |> to_string in
+    let context_var = Var(Ident(List.nth (List.nth (List.nth (List.nth edge 3 |> to_list) 1 |> to_list) 1 |> to_list) 1 |> to_string), None) in
+    let context_clause =
+      try
+        Hashtbl.find map context_var
+      with
+      | Not_found ->
+        failwith "Context clause not found in map"
+    in
+    Enter_clause(Core_ast.Var(Ident(param),None), Core_ast.Var(Ident(arg),None), Clause(context_var, context_clause))
+  else if node_type = "Exit_clause" then
+    let old_program_pt = List.nth (List.nth (List.nth edge 1 |> to_list) 1 |> to_list) 1 |> to_string in
+    let new_program_pt = List.nth (List.nth (List.nth edge 2 |> to_list) 1 |> to_list) 1 |> to_string in
+    let context_var = Var(Ident(List.nth (List.nth (List.nth (List.nth edge 3 |> to_list) 1 |> to_list) 1 |> to_list) 1 |> to_string), None) in
+    let context_clause =
+      try
+        Hashtbl.find map context_var
+      with
+      | Not_found ->
+        failwith "Context clause not found in map"
+    in
+    Exit_clause(Core_ast.Var(Ident(old_program_pt),None), Core_ast.Var(Ident(new_program_pt),None), Clause(context_var, context_clause))
   else
     failwith "Unrecognized node type in json ddpa graph"
 ;;
@@ -80,11 +118,10 @@ let rec initialize_graph (map: (var,clause_body) Hashtbl.t) (elements:Yojson.Bas
     initialize_graph map tail graph
 ;;
 
-let mayBeTop context_stack context : bool =
+let mayBeTop (context_stack:Core_ast.clause Stack.t) (context:Core_ast.clause) : bool =
   if Stack.is_empty context_stack then
     true
-  else
-  if Stack.top = context then
+  else if Stack.top context_stack = context then
     true
   else
     false
@@ -101,6 +138,11 @@ let rec lookup lookup_stack (node:annotated_clause) context_stack graph phi: (Co
     with
     | Not_found -> failwith "not in graph"
   in
+  (* print_endline ("\nCurrent lookup variable: " ^ string_of_var cur_var); *)
+  (* print_endline ("Current node: " ^ string_of_annotated_clause node); *)
+  (* print_endline ("a1 node: " ^ string_of_annotated_clause a1); *)
+  (* print_stack lookup_stack; *)
+  (* print_context_stack context_stack; *)
   match a1 with
   | Unannotated_clause(cl) ->
     let Clause(x, body) = cl in
@@ -113,7 +155,11 @@ let rec lookup lookup_stack (node:annotated_clause) context_stack graph phi: (Co
       begin
         match body with
         | Value_body(v) ->
-          v, phi@[Binary_formula(Var_formula(x), Binary_operator_equal_to, Value_formula(v))]
+          if Stack.length lookup_stack > 1 then
+            let _ = Stack.pop lookup_stack in
+            lookup lookup_stack a1 context_stack graph phi
+          else
+            v, phi@[Binary_formula(Var_formula(x), Binary_operator_equal_to, Value_formula(v))]
         | Var_body(var) ->
           let _ = Stack.pop lookup_stack in
           Stack.push (var, "placeholder") lookup_stack;
@@ -125,10 +171,9 @@ let rec lookup lookup_stack (node:annotated_clause) context_stack graph phi: (Co
           let existence_formula = Binary_formula(negCover, Binary_operator_bool_or, posCover) in
           Value_int(0),phi@[existence_formula] (* pretty sure the value here is garbage - well what if we depend on it later? *)
         | Appl_body(_,_) ->
-          failwith "appl body encountered"
+          failwith "appl body encountered - this should never happen I think"
         | Unary_operation_body(op, v1) ->
           let _ = Stack.pop lookup_stack in
-          (* let lookup_stack1 = lookup_stack in  *)
           Stack.push (v1, "placeholder") lookup_stack;
           let v2, phi2 = lookup lookup_stack a1 context_stack graph phi in
           begin
@@ -144,12 +189,13 @@ let rec lookup lookup_stack (node:annotated_clause) context_stack graph phi: (Co
           end
         | Binary_operation_body(v1, op, v2) ->
           let _ = Stack.pop lookup_stack in
-          let lookup_stack1 = lookup_stack in
+          let lookup_stack1 = Stack.copy lookup_stack in
           Stack.push (v1, "placeholder") lookup_stack1;
+          let context_stack1 = Stack.copy context_stack in
           let left_value, phi1 = lookup lookup_stack1 a1 context_stack graph phi in
-          let lookup_stack2 = lookup_stack in
-          Stack.push (v2, "placeholder") lookup_stack1;
-          let right_value, phi2 = lookup lookup_stack2 a1 context_stack graph phi in
+          let lookup_stack2 = Stack.copy lookup_stack in
+          Stack.push (v2, "placeholder") lookup_stack2;
+          let right_value, phi2 = lookup lookup_stack2 a1 context_stack1 graph phi in
           let phi = phi@phi1@phi2 in
           begin
             match op with
@@ -217,26 +263,84 @@ let rec lookup lookup_stack (node:annotated_clause) context_stack graph phi: (Co
         | _ ->
           failwith "not implemented yet"
       end
-  | Enter_clause(_) ->
-    failwith "todo"
-  (* | Exit_clause(original_program_point, new_program_point, (Clause(_, body) as cl)) -> *)
-  | Exit_clause(_) -> failwith "dodo"
-    (* begin
+  | Enter_clause(param, arg, (Clause(_, body) as cl)) ->
+    begin
       match body with
       | Appl_body(xf, xn) ->
-        let new_stack = Stack.create () in
-        Stack.push (xf, "placeholder") new_stack;
-        let f,_ = lookup new_stack (Unannotated_clause(cl)) context_stack graph phi in
-        let rv_xf = rv_of_function f in
-        let _ = Stack.pop lookup_stack in
-        Stack.push (rv_xf, "placeholder") lookup_stack;
-        Stack.push cl context_stack;
-        lookup lookup_stack a1 context_stack graph (phi@[Binary_formula(Var_formula(cur_var),Binary_operator_equal_to, Var_formula(xn))])
+        if cur_var = param then
+          let new_stack = Stack.create () in
+          Stack.push (xf, "placeholder") new_stack;
+          let new_context_stack = context_stack in
+          let _ = Stack.pop context_stack in
+          let _ =
+            try
+              lookup new_stack a1 new_context_stack graph phi
+            with
+            | Not_found -> failwith ("function " ^ (string_of_var xf) ^ "not found")
+          in
+          let _ = Stack.pop lookup_stack in
+          Stack.push (arg, "placeholder") lookup_stack;
+          if mayBeTop context_stack cl then
+            (
+              lookup lookup_stack a1 new_context_stack graph (phi@[Binary_formula(Var_formula(cur_var),Binary_operator_equal_to, Var_formula(xn))])
+            )
+          else
+            failwith "context didn't match?"
+        else
+          let _ = Stack.push (xf, "placeholder") lookup_stack in
+          let _ = Stack.pop context_stack in
+          (* lookup lookup_stack a1 (context_stack) graph (phi@[Binary_formula(Var_formula(cur_var), Binary_operator_equal_to, Var_formula(xn))]) *)
+          lookup lookup_stack a1 (context_stack) graph phi
       | Conditional_body(_,_,_,_) ->
         failwith "conditional todo"
       | _ ->
         failwith "exit clause context is not appl or cond"
-    end *)
+    end
+  | Exit_clause(original_program_point, new_program_point, (Clause(_, body) as cl)) ->
+    begin
+      match body with
+      | Appl_body(xf, _) ->
+        if original_program_point = cur_var then
+          let new_stack = Stack.create () in
+          Stack.push (xf, "placeholder") new_stack;
+          let _ =
+            try
+              lookup new_stack (Unannotated_clause(cl)) context_stack graph phi
+            with
+            | Not_found -> failwith ("function " ^ (string_of_var xf) ^ "not found")
+          in
+          (* let rv_xf = rv_of_function f in *)
+          let _ = Stack.pop lookup_stack in
+          Stack.push (new_program_point, "placeholder") lookup_stack;
+          if mayBeTop context_stack cl then
+            (
+              Stack.push cl context_stack;
+              print_endline ("\nFunction found, proceeding");
+              lookup lookup_stack a1 context_stack graph (phi@[Binary_formula(Var_formula(cur_var),Binary_operator_equal_to, Var_formula(new_program_point))])
+            )
+          else
+            failwith "context didn't match?"
+        else
+          failwith "exit clause: vars didn't match"
+      | Conditional_body(x,p,Function_value(_,Expr(f1)),Function_value(_,Expr(f2))) ->
+        let new_lookup_stack = Stack.copy lookup_stack in
+        let _ = Stack.pop lookup_stack in
+        Stack.push (x,"placehodler") new_lookup_stack;
+        let new_context_stack = Stack.copy context_stack in
+        let xv,_ = lookup new_lookup_stack a1 new_context_stack graph phi in
+        (* evaulate *)
+        let rv_of_function =
+          if matches xv p then
+            rv f1
+          else
+            rv f2
+        in
+        Stack.push (rv_of_function,"placehoder") lookup_stack;
+        Stack.push cl context_stack;
+        failwith "d"
+      | _ ->
+        failwith "exit clause context is not appl or cond"
+    end
 
   (* arg, pattern, then, else *)
   | Conditional_enter_clause(_) ->
@@ -244,10 +348,10 @@ let rec lookup lookup_stack (node:annotated_clause) context_stack graph phi: (Co
   (* Conditional_enter_clause, x from x = conditional *)
   | Conditional_exit_clause(_,_,_,_,_,_) ->
     failwith "todo"
-  | Start_clause(_) ->
-    failwith "todo"
+  | Start_clause(_)
   | End_clause(_) ->
-    failwith "todo"
+    (* think we just skip *)
+    lookup lookup_stack a1 context_stack graph phi
   | Junk_clause ->
     failwith "todo"
 ;;
@@ -283,6 +387,14 @@ let eval (Expr(cls)) : Core_ast.var * value Core_interpreter.Environment.t * for
         | _ ->
           failwith "last line was not an alias"
       end
+  in
+
+  let _ =
+    try
+      let _ = find_clause_by_var graph starting_program_point in
+      "placeholder"
+    with
+    | Not_found -> failwith "unreachable" (* assumes if node isn't in graph its unreachable *)
   in
 
   Stack.push (starting_program_point, "placeholder") lookup_stack;
