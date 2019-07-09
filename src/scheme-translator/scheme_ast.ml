e1fc12d54a973b4f6466dbfbdc1578a180b62530
Earl WU <EarlWu96@gmail.com>
2019-07-09 14:57:46 -0400

    Hand translation for the win

--- src/scheme-translator/scheme_ast.ml
+++
@@ -1,8 +0,0 @@
-open Batteries;;
-(* open Jhupllib;; *)
-
-type ident = Ident of string [@@deriving eq, ord, show, to_yojson];;
-
-type atom_literal = AString of string | AInt of int;;
-
-type sexpr = Atom of atom_literal | Group of sexpr list;;
