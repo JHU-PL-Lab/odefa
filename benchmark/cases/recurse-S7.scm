3c5140965551392027aa99aaf0604aca188598a6
Zachary Palmer <zep_git@bahj.com>
2019-08-08 09:27:08 -0400

    Two more recursion cases.

---
+++ benchmark/cases/recurse-S7.scm
@@ -0,0 +1,18 @@
+(define (pathological x) (
+    if (eq? x x) (pathological x) (
+    if (eq? x x) (pathological x) (
+    if (eq? x x) (pathological x) (
+    if (eq? x x) (pathological x) (
+    if (eq? x x) (pathological x) (
+    if (eq? x x) (pathological x) (
+    if (eq? x x) (pathological x) (
+    0
+    )
+    )
+    )
+    )
+    )
+    )
+    )
+))
+(pathological 5)
