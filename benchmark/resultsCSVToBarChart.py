0ff830ca37ffe7fc2d040d5dd1288ff582119b94
Zachary Palmer <zep_git@bahj.com>
2019-09-06 17:06:51 -0400

    Allow benchmark CSVs to produce wide bar charts.

--- benchmark/resultsCSVToBarChart.py
+++ benchmark/resultsCSVToBarChart.py
@@ -22,6 +22,8 @@ def parse_args():
     parser.add_argument("--output-dir", metavar="OUTPUT_DIR", type=str,
                         default="results",
                         help="The directory in which to write the charts.")
+    parser.add_argument("--horizontal", action="store_true", default=False,
+                        help="Produce a horizontally-oriented bar graph.")
     return parser.parse_args()
 
 def read_csvs(csv_files):
@@ -68,21 +70,29 @@ def read_csvs(csv_files):
                            }
     return ret
 
-def produce_bar_chart_from(output_dir, name, group_data):
+def produce_bar_chart_from(output_dir, name, group_data, is_horizontal):
     """
     Produces a bar chart from the provided experiment data.
     """
-    cases = list(reversed(group_data["cases"]))
+    cases = list(group_data["cases"])
+    if not is_horizontal:
+        cases.reverse()
     analyses = group_data["analyses"]
     analyses = analyses_in_preferred_order(analyses)
     analyses.reverse()
-    figure = mplot.figure(figsize=(10,0.75*len(cases)*len(analyses)))
+    bar_size = 6
+    area_size = 0.75*len(cases)*len(analyses)
+    size = (bar_size,area_size) if not is_horizontal else (area_size,bar_size)
+    figure = mplot.figure(figsize=size)
     ax = figure.add_axes([0.2,0.2,0.7,0.7])
     MIN_TIME = 1
     MAX_TIME = 1000 * 60 * 30 # 30 minutes
-    ax.set_xlim(MIN_TIME, MAX_TIME)
+    if is_horizontal:
+        ax.set_ylim(MIN_TIME, MAX_TIME)
+    else:
+        ax.set_xlim(MIN_TIME, MAX_TIME)
     bar_group_area = 2
-    bar_group_height = 0.7*bar_group_area
+    bar_group_size = 0.7*bar_group_area
     analysis_colors = {"ddpa":               (0.00,0.62,0.45), # sea green
                        "p4f":                (0.80,0.47,0.65), # reddish purple
                        "kplume":             (0.34,0.71,0.91), # sky blue
@@ -91,17 +101,24 @@ def produce_bar_chart_from(output_dir, name, group_data):
                        "boomerangOriginal":  (0.90,0.62,0.00), # orange
                       }
     for case_idx, case in enumerate(cases):
-        base_position = case_idx * bar_group_area - 0.5*bar_group_height
+        base_position = case_idx * bar_group_area - 0.5*bar_group_size
         for analysis_idx, analysis in enumerate(analyses):
-            bar_height = bar_group_height/len(analyses)
-            bar_position = base_position + bar_height * (analysis_idx+0.5)
+            bar_size = bar_group_size/len(analyses)
+            bar_position = base_position + bar_size * (analysis_idx+0.5)
             time = group_data["results"][case][analysis]["time"]
             if time is None:
-                bar = ax.barh([bar_position],
-                              [MAX_TIME],
-                              bar_height,
-                              edgecolor='black',
-                              label=analysis)[0]
+                if is_horizontal:
+                    bar = ax.bar([bar_position],
+                                 [MAX_TIME],
+                                 bar_size,
+                                 edgecolor='black',
+                                 label=analysis)[0]
+                else:
+                    bar = ax.barh([bar_position],
+                                  [MAX_TIME],
+                                  bar_size,
+                                  edgecolor='black',
+                                  label=analysis)[0]
                 bar.set_color(analysis_colors.get(analysis) or "Black")
                 bar.set_edgecolor("Black")
                 bar.set_hatch("//")
@@ -109,30 +126,47 @@ def produce_bar_chart_from(output_dir, name, group_data):
                 print("Time <{}ms reported for {} on {}".format(
                         MIN_TIME, analysis, case))
             else:
-                bar = ax.barh([bar_position],
-                              [time],
-                              bar_height,
-                              label=analysis)[0]
+                if is_horizontal:
+                    bar = ax.bar([bar_position],
+                                 [time],
+                                 bar_size,
+                                 label=analysis)[0]
+                else:
+                    bar = ax.barh([bar_position],
+                                  [time],
+                                  bar_size,
+                                  label=analysis)[0]
                 bar.set_color(analysis_colors.get(analysis) or "Black")
+                if is_horizontal:
+                    xy=(bar.get_x() + bar.get_width() / 2, bar.get_height())
+                else:
+                    xy=(bar.get_width(), bar.get_y() + bar.get_height() / 2)
                 ax.annotate("{:d}".format(int(time)),
-                            xy=(bar.get_width(),
-                                bar.get_y() + bar.get_height() / 2),
-                            xytext=(3,0),
+                            xy=xy,
+                            xytext=(0,3) if is_horizontal else (3,0),
                             textcoords="offset points",
-                            ha="left",
-                            va="center",
+                            ha="center" if is_horizontal else "left",
+                            va="bottom" if is_horizontal else "center",
                             fontsize=6)
-        ax.set_xlabel("Time (ms)")
-        ax.set_xscale("log")
-        ax.set_yticks(list(map(lambda n: n * bar_group_area, range(len(cases)))))
-        ax.set_yticklabels(cases)
+        if is_horizontal:
+            ax.set_ylabel("Time (ms)")
+            ax.set_yscale("log")
+            ax.set_xticks(list(map(lambda n: n * bar_group_area, range(len(cases)))))
+            ax.set_xticklabels(cases)
+        else:
+            ax.set_xlabel("Time (ms)")
+            ax.set_xscale("log")
+            ax.set_yticks(list(map(lambda n: n * bar_group_area, range(len(cases)))))
+            ax.set_yticklabels(cases)
         # for tick in ax.yaxis.get_major_ticks()[0::2]:
         #     tick.set_pad(12)
+    #if not is_horizontal:
+        #analyses = list(reversed(analyses))
     ax.legend([
         mplot.Rectangle((0,0),1,1,fc=analysis_colors[analysis])
-        for analysis in reversed(analyses)
+            for analysis in analyses
         ],
-        list(reversed(analyses))
+        analyses
     )
     figure.savefig(output_dir + os.sep + name + "-bars.svg")
 
@@ -141,6 +175,7 @@ def main():
     data = read_csvs(args.csv_files)
     for group in data:
         if data[group]["cases"] and data[group]["analyses"]:
-            produce_bar_chart_from(args.output_dir, group, data[group])
+            produce_bar_chart_from(args.output_dir, group, data[group],
+                                   args.horizontal)
 
 main()
