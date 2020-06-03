9a2771beb3d98aaa870532a8e365fe900aa46d65
arbipher <arbipher@gmail.com>
2020-06-03 15:37:00 -0400

    add the extra testcases in the script

--- benchmark-test-generation/benchmark.ml
+++ benchmark-test-generation/benchmark.ml
@@ -44,40 +44,49 @@ let benchmark test n : unit t =
   (* time gtimeout --foreground 1m ls *)
   >> stdout_to ~append:() test_result (call [
       "/usr/bin/time"; 
-        "-o"; test_time_result; 
-        "-a";
-        "-f"; "%e\n%Uuser %Ssystem %Eelapsed %PCPU (%Xtext+%Ddata %Mmax)k %Iinputs+%Ooutputs (%Fmajor+%Rminor)pagefaults %Wswaps";
+      "-o"; test_time_result; 
+      "-a";
+      "-f"; "%e\n%Uuser %Ssystem %Eelapsed %PCPU (%Xtext+%Ddata %Mmax)k %Iinputs+%Ooutputs (%Fmajor+%Rminor)pagefaults %Wswaps";
       "/usr/bin/timeout"; 
-        "--foreground"; timeout_limit;
+      "--foreground"; timeout_limit;
       "./test_generator"; 
-        "-t"; "target"; 
-        "-r"; "1"; 
-        "-b"; "true"; 
-        test_path])
+      "-t"; "target"; 
+      "-r"; "1"; 
+      "-b"; "true"; 
+      test_path])
   >> echo @@ " done - " ^ (string_of_int n)
 (* |- run "tee" ["-a"; result_file] *)
+
+let extra_case : unit t =
+  let extra_result = working_path ^ "extra.txt"
+  and file_path = "benchmark-test-generation/other_cases/input_list_sum_add_build.natodefa" in
+  stdout_to ~append:() extra_result (
+    call ["./test_generator"; file_path; "-t"; "target"; "-r"; "4" ]
+  )
+  >> echo @@ " done - extra"
+
 
 let stat : unit t =
   let table = working_path ^ "0table.txt" in
   List.iter testcases ~f:(fun testcase ->
-    stdout_to ~append:() table
-    (run "echo" [testcase]
-     >> run "awk" ["BEGIN {sum=0;count=0} {if (NR%2==1) {sum=sum+$1; count=count+1}}  END {print sum/count} "; working_path ^ testcase ^ ".time.txt"]
-     >> run "awk" ["FNR == 2"; working_path ^ testcase ^ "_1.txt"]
-     (* >> run "awk" ["BEGIN {sum=0;count=0} {FNR==NR; if (NR==2) {sum=sum+$1; count=count+1}} {next} {print sum/count}' result/evaluate-2020-05-22-23:55:15Z/*.result.txt"] *)
+      stdout_to ~append:() table
+        (run "echo" [testcase]
+         >> run "awk" ["BEGIN {sum=0;count=0} {if (NR%2==1) {sum=sum+$1; count=count+1}}  END {print sum/count} "; working_path ^ testcase ^ ".time.txt"]
+         >> run "awk" ["FNR == 2"; working_path ^ testcase ^ "_1.txt"]
+         (* >> run "awk" ["BEGIN {sum=0;count=0} {FNR==NR; if (NR==2) {sum=sum+$1; count=count+1}} {next} {print sum/count}' result/evaluate-2020-05-22-23:55:15Z/*.result.txt"] *)
+        )
     )
-  )
 
 let collect_result : unit t =
   (* let awk_job = "awk 'BEGIN {sum=0;count=0} {if (NR%2==1) {sum=sum+$1;count=count+1}}  END {print sum/count}' $f"
-  in  
-  in (run ("for f in " ^ working_path ^ "*.time.txt; do " ^ awk_job ^ "; done") [])
-  (run "./script/echo.sh" [])
-  >>  *)
+     in  
+     in (run ("for f in " ^ working_path ^ "*.time.txt; do " ^ awk_job ^ "; done") [])
+     (run "./script/echo.sh" [])
+     >>  *)
   (* let test_name = String.(sub testcase 0 (rindex testcase '.')) in *)  
   (run "date" ["-u"; "+%Y-%m-%d-%H:%M:%SZ"])
   |- (run "xargs" ["-I"; "%"; "mv"; working_path; "result/evaluate-%"])
-    
+
 let rec countdown n task : unit t =
   if n > 0 then
     task n
@@ -91,17 +100,5 @@ let main  =
     prepare
     >> List.iter testcases ~f:(fun task -> countdown repeat_n (benchmark task))
     >> stat
+    >> extra_case
     >> collect_result)
-
-(* 
-awk '
-BEGIN ;
-{sum+=}
-END {print sum}
-'
-
-
-for f in *.ml do echo head -n 1 done
-
-
-*)
