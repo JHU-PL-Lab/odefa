94a02e4086ea7fcbcb2d6650572dfc01fd5abd48
arbipher@gmail.com <arbipher@gmail.com>
2020-06-02 21:06:35 -0400

    update benchmark

--- benchmark-test-generation/benchmark.ml
+++ benchmark-test-generation/benchmark.ml
@@ -13,7 +13,7 @@ let testcases = [
   "input_map.natodefa";
   "input_mj09.natodefa";
   "input_sat_1.natodefa";
-  "smbc_fold0.natodefa";
+  "smbc_fold0s.natodefa";
   "smbc_gen_list_len.natodefa";
   "smbc_long_rev_sum3.natodefa";
   "smbc_pigeon.natodefa";
