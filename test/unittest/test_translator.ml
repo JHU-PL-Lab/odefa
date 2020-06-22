4231df737b2244168d71c7a8f20675e17b9e3103
kelvinqian00 <kelvinqian2@gmail.com>
2020-06-22 13:21:11 -0400

    Add more unit tests

--- test/unittest/test_translator.ml
+++ test/unittest/test_translator.ml
@@ -57,21 +57,36 @@ _add_test "pattern_variables" @@ fun _ ->
 
 (* TODO: Test flattening *)
 
-(* TODO: Test constraining *)
+(* Test constraining *)
 
-let format_expected expected_str =
-  expected_str
-  |> Str.global_replace (Str.regexp ";\n[ ]*") ";\n  "
-  |> (fun s -> "\n  " ^ s ^ "\n")
-;;
-
-let calculate_actual input =
+let calculate_actual (input: string) : Ast.expr =
   let open TranslationMonad in
   let expr = parse_odefa input in
   let Ast.Expr(clist) = expr in
   let tc = new_translation_context () in
   let new_clist = run tc (condition_clauses clist) in
-  show_expr @@ Ast.Expr(new_clist)
+  Ast.Expr(new_clist)
+;;
+
+let eliminate_tildes (str: string) : string =
+  let re = Str.regexp "~\\([0-9]\\)" in
+  Str.global_replace re "___\\1" str
+;;
+
+let test_exprs (input: string) (expected: string) : unit =
+  let actual_expr = calculate_actual input in
+  let actual_expr' =
+    actual_expr
+    |> show_expr
+    |> eliminate_tildes
+    |> parse_odefa
+  in
+  let expected_expr = parse_odefa @@ eliminate_tildes expected in
+  assert_equal
+    ~printer:show_expr
+    ~cmp:(=)
+    expected_expr
+    actual_expr'
 ;;
 
 _add_test "constrain_arithmetic" @@ fun _ ->
@@ -80,18 +95,16 @@ _add_test "constrain_arithmetic" @@ fun _ ->
      b = input;
      binop = a + b;"
   in
-  let expected = format_expected @@ 
+  let expected =
     "a = input;
      b = input;
      m1~0 = a ~ int;
      m2~1 = b ~ int;
      m~2 = m1~0 and m2~1;
-     constrain_binop~3 = m~2 ? (binop = a + b) : (ab~4 = abort)"
+     constrain_binop~3 = m~2 ? (binop = a + b) : (ab~4 = abort);"
   in
-  let actual = calculate_actual input in
-  assert_equal ~printer:Fun.id expected actual
+  test_exprs input expected
 ;;
-
 
 _add_test "constrain_boolean" @@ fun _ ->
   let input =
@@ -99,7 +112,7 @@ _add_test "constrain_boolean" @@ fun _ ->
      b = false;
      binop = a and b;"
   in
-  let expected = format_expected @@
+  let expected =
     "a = true;
      b = false;
      m1t~0 = a ~ true;
@@ -111,8 +124,7 @@ _add_test "constrain_boolean" @@ fun _ ->
      m~6 = m1~4 and m2~5;
      constrain_binop~7 = m~6 ? (binop = a and b) : (ab~8 = abort)"
   in
-  let actual = calculate_actual input in
-  assert_equal ~printer:Fun.id expected actual
+  test_exprs input expected
 ;;
 
 _add_test "constrain_application" @@ fun _ ->
@@ -122,15 +134,12 @@ _add_test "constrain_application" @@ fun _ ->
      appl = f a;"
   in
   let expected =
-    "\n" ^
-    "  f = fun x -> (\n" ^
-    "  ret = x);\n" ^
-    "  a = input;\n" ^
-    "  m~0 = f ~ fun;\n" ^
-    "  constrain_appl~1 = m~0 ? (appl = f a) : (ab~2 = abort)\n"
+    "f = fun x -> (ret = x);
+     a = input;
+     m~0 = f ~ fun;
+     constrain_appl~1 = m~0 ? (appl = f a) : (ab~2 = abort)"
   in
-  let actual = calculate_actual input in
-  assert_equal ~printer:Fun.id expected actual
+  test_exprs input expected
 ;;
 
 _add_test "constrain_projection" @@ fun _ ->
@@ -139,14 +148,13 @@ _add_test "constrain_projection" @@ fun _ ->
      r = {label=a};
      proj = r.label;"
   in
-  let expected = format_expected @@
+  let expected =
     "a = input;
      r = {label=a};
      m~0 = r ~ {label};
      constrain_proj~1 = m~0 ? (proj = r.label) : (ab~2 = abort)"
   in
-  let actual = calculate_actual input in
-  assert_equal ~printer:Fun.id expected actual
+  test_exprs input expected
 ;;
 
 _add_test "constrain_conditional" @@ fun _ ->
@@ -154,24 +162,46 @@ _add_test "constrain_conditional" @@ fun _ ->
     "pred = true;
      cond = pred ? (res1 = true) : (res2 = false);"
   in
-  let expected = 
-    "\n" ^
-    "  pred = true;\n" ^
-    "  cond = pred ? (\n" ^
-    "                   mt~0 = pred ~ true;\n" ^
-    "                   iftrue~2 = mt~0 ? (res1 = true) : (ab~4 = abort)\n" ^
-    "                  ) : (\n" ^
-    "                         mf~1 = pred ~ false;\n" ^
-    "                         iffalse~3 = mf~1 ? (res2 = false) : (ab~5 = abort)\n" ^
-    "                        )\n"    
+  let expected =
+    "pred = true;
+     cond = pred ? (mt~0 = pred ~ true;
+                    iftrue~2 = mt~0 ? (res1 = true) : (ab~4 = abort)
+               ) : (mf~1 = pred ~ false;
+                    iffalse~3 = mf~1 ? (res2 = false) : (ab~5 = abort)
+               )"
   in
-  let actual = calculate_actual input in
-  assert_equal ~printer:Fun.id expected actual
+  test_exprs input expected
 ;;
 
-(* _add_test "constrain_serial" @@ fun _ -> () *)
-
-(* _add_test "constrain_in_fun" @@ fun_ -> () *)
+_add_test "constrain_in_fun" @@ fun _ ->
+  let input =
+    "f = fun x -> (
+       g = fun y -> (rety = x + y);
+       retx = g);
+     a = input;
+     b = input;
+     appl1 = f a;
+     appl2 = appl1 b;"
+  in
+  let expected = 
+    "f = fun x -> (
+       g = fun y -> ( 
+         m1~0 = x ~ int;
+         m2~1 = y ~ int;
+         m~2 = m1~0 and m2~1;
+         constrain_binop~3 = m~2 ? (rety = x + y) : (ab~4 = abort));
+       retx = g);
+     a = input;
+     b = input;
+     m~5 = f ~ fun;
+     constrain_appl~6 = m~5 ? (appl1 = f a;
+                               m~7 = appl1 ~ fun;
+                               constrain_appl~8 = m~7 ? (appl2 = appl1 b)
+                                                      : (ab~9 = abort)
+                          ) : (ab~10 = abort)"
+  in
+  test_exprs input expected
+;;
 
 (* **** For main test module **** *)
 
